4eeacec76c9dbfa4861569e6501699000110273b
Abhishek Gokal <abhigokal@gmail.com>
2021-06-17 13:54:05 +0530

    Add files via upload

---
+++ DragTouch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragTouch : MonoBehaviour
+{
+  
+    void Update()
+    {
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Vector3 touchPos = Input.GetTouch(0).position;
+            gameObject.transform.position = touchPos;
+        }
+        else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            // Finger was picked up
+            // Do something
+        }
+    }
+}
---
+++ TeleportOnTouch.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TeleportOnTouch : MonoBehaviour
+{
+    void Update()
+    {    
+        // Teleport gameobject to touch position
+        if (Input.touchCount > 0)
+        {
+            Vector3 touchPos = Input.GetTouch(0).position;
+            gameObject.transform.position = touchPos;
+        }      
+    }
+}
