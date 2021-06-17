4a2e105266babde325266831b838729e24f8d7fd
Abhishek Gokal <abhigokal@gmail.com>
2021-06-17 13:47:13 +0530

    Add files via upload

---
+++ SingleTap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleTap : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            // Player has tapped the screen
+            // Do something...
+        }
+    }
+}
---
+++ TapOnLeftOrRightSide.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapOnLeftOrRightSide : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touch_pos = Camera.main.ScreenToWorldPoint(touch.position);
+            // Get left or right position
+            if (touch_pos.x > 0)
+            {
+                // Player has tapped on the right half of the screen
+            }
+            else if (touch_pos.x < 0)
+            {
+                // Player has tapped on the right half of the screen
+            }
+
+            
+            // Get top or bot position
+            if (touch_pos.y > 0)
+            {
+                // Player has tapped on the top half of the screen
+            }
+            else if (touch_pos.y < 0)
+            {
+                // Player has tapped on the bottom half of the screen
+            }
+        }    
+    }
+}
